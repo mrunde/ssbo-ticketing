c945201db5ecc5e5e0130d93cd96c65b500f9d73
Marius <marius.runde@uni-muenster.de>
2014-02-07 16:13:20 +0100

    Create demo data for the DB

---
+++ db/demo_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `ssbo-ticketing`.`tickets`
+(`code`, `firstname`, `lastname`, `street`, `housenumber`, `postalcode`, `town`, `date`)
+VALUES
+('1911332118324935400', 'Max', 'Mustermann', 'Musterstraße', '1a', '12345', 'Musterdorf', '2014-02-07 16:02:41');
