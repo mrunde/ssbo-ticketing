1795e269ff87ca18a5e1b9ba5245a772fc569c5c
Marius <marius.runde@uni-muenster.de>
2014-02-07 16:06:52 +0100

    Compare code of URL with codes from DB Also create table for MySQL database

--- index.php
+++ index.php
@@ -21,24 +21,58 @@
 	</head>
 
 	<body>
-		<div>
-			<header>
-				<h1>SSBO Ticketing</h1>
-			</header>
+		<header>
+			<h1>SSBO Ticketing</h1>
+		</header>
 
-			<div id="qrcode">
-				<!-- QRCode will be placed here -->
-			</div>
-			<script type="text/javascript">
-				// Create the QRCode
-				new QRCode(document.getElementById("qrcode"), getParam("code"));
-			</script>
-
-			<footer>
-				<p>
-					&copy; Copyright 2014 by Marius Runde
-				</p>
-			</footer>
+		<div id="qrcode">
+			<!-- QRCode will be placed here, if the code is valid. If not a message will be prompted to the user. -->
 		</div>
+		
+		<?php
+			// Control the code
+			$code = $_GET['code'];
+	
+			// Open a connection to the MySQL server and select the database
+			require_once 'db_info.php';
+			$connection = mysql_connect($db_host, $db_username, $db_password) or die('Keine Verbindung möglich. Benutzername oder Passwort sind falsch');
+			mysql_select_db($db_database) or die('Die Datenbank existiert nicht.');
+	
+			// Select the row with the correct user_id
+			$query = "SELECT * FROM tickets WHERE code = '" . $code . "' LIMIT 1";
+			$result = mysql_query($query);
+	
+			$firstname = "";
+			while ($row = mysql_fetch_object($result)) {
+				$firstname = $row -> firstname;
+			}
+			
+			if ($firstname != "") {
+				// Code is correct
+				echo '
+		<script type="text/javascript">
+			// Create the QRCode
+			new QRCode(document.getElementById("qrcode"), getParam("code"));
+		</script>
+				';
+					} else {
+				// Code is wrong
+				echo '
+		<script type="text/javascript">
+			document.getElementById("qrcode").innerHTML = "Dein Code konnte nicht in der Datenbank gefunden werden. Bitte überprüfe deine Eingaben. Wenn du glaubst, dass hier ein Fehler vorliegt, melde dich bitte beim Veranstalter.";
+		</script>
+				';
+				
+			}
+	
+			// Close connection to MySQL server
+			mysql_close($connection);
+		?>
+
+		<footer>
+			<p>
+				&copy; Copyright 2014 by Marius Runde
+			</p>
+		</footer>
 	</body>
 </html>
