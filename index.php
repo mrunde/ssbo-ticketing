a4de2c2e8eee878a4318b614d945d66d1216fcd5
Marius Runde <marius.runde@uni-muenster.de>
2014-04-07 17:19:10 +0200

    Update index.php to use stylesheet and images now

--- index.php
+++ index.php
@@ -14,7 +14,10 @@
 		<title>SSBO Ticketing</title>
 		<meta name="description" content="SSBO Ticketing System" />
 		<meta name="author" content="Marius Runde" />
-
+		
+		<!-- Stylesheet -->
+		<link rel="stylesheet" href="style.css" />
+		
 		<!-- Favicon -->
 		<link rel="shortcut icon" href="img/favicon.png" />
 		
@@ -46,69 +49,88 @@
 				</li>
 			</ul>
 		</div>
-
-		<div id="qrcode">
-			<!-- QRCode will be placed here, if the code is valid. If not a message will be prompted to the user. -->
+		
+		<div style="position: relative; left: 0; top: 0;">
+			<div id="ticket">
+				<!-- Ticket will be placed here, if the code is valid. -->
+			</div>
+			<div id="qrcode">
+				<!-- QRCode will be placed here, if the code is valid. -->
+			</div>
 		</div>
 		
 		<?php
 			// Control the code
-			$code = $_GET['code'];
-	
-			// Open a connection to the MySQL server and select the database
-			require_once 'db_info.php';
-			$connection = mysql_connect($db_host, $db_username, $db_password) or die('Keine Verbindung möglich. Benutzername oder Passwort sind falsch');
-			mysql_select_db($db_database) or die('Die Datenbank existiert nicht.');
-	
-			// Select the row with the correct user_id
-			$query = "SELECT * FROM tickets WHERE code = '" . $code . "' LIMIT 1";
-			$result = mysql_query($query);
-	
-			while ($row = mysql_fetch_object($result)) {
-				$used = $row -> used;
+			if (isset($_GET['code'])) {
+				$code = $_GET['code'];
+				
+				// Open a connection to the MySQL server and select the database
+				if (! @include_once('db_info.php')) { // @ - to suppress warnings 
+  					echo '<h1>500 Internal Server Error!</h1><br/>
+  					<b>Error message:</b> db_info.php does not exist!';
+  				} else {
+					$connection = mysql_connect($db_host, $db_username, $db_password) or die('Keine Verbindung möglich. Benutzername oder Passwort sind falsch');
+					mysql_select_db($db_database) or die('Die Datenbank existiert nicht.');
+			
+					// Select the row with the correct user_id
+					$query = "SELECT * FROM tickets WHERE code = '" . $code . "' LIMIT 1";
+					$result = mysql_query($query);
+			
+					while ($row = mysql_fetch_object($result)) {
+						$used = $row -> used;
+					}
+					
+					if (isset($used) && $used == FALSE) {
+						if (isset($_SESSION['logged_in'])) {
+							// Update the code from unused to used
+							$query_update = "UPDATE tickets SET used = true WHERE code = '" . $code . "'";
+							mysql_query($query_update);
+						}
+						
+						// Code is correct
+						echo '
+				<script type="text/javascript">
+					// Create the QRCode
+					document.getElementById("ticket").innerHTML = "<img src=\"img/ticket.png\" />";
+					new QRCode(document.getElementById("qrcode"), {
+						text: getParam("code"),
+						width: 256,
+						height: 256,
+						colorDark : "#0000ff",
+						colorLight : "#ffffff",
+						correctLevel : QRCode.CorrectLevel.H
+					})
+				</script>
+						';
+					} elseif (isset($used) && $used == TRUE) {
+						// Code has been used before
+						echo '
+				<script type="text/javascript">
+					document.getElementById("qrcode").innerHTML = "<img src=\"img/not_ok.png\" /><br/>" +
+						"<h3>Dieser Code wurde bereits verwendet!</h3>";
+				</script>
+						';
+					} else {
+						// Code is wrong	
+						echo '
+				<script type="text/javascript">
+					document.getElementById("qrcode").innerHTML = "<img src=\"img/fake.png\" /><br/>" +
+						"<h3>Dein Code konnte nicht in der Datenbank gefunden werden.<br/>" +
+						"Bitte überprüfe deine Eingaben.<br/>" +
+						"Wenn du glaubst, dass hier ein Fehler vorliegt, melde dich bitte beim Veranstalter.</h3>";
+				</script>
+						';
+						
+					}
+			
+					// Close connection to MySQL server
+					mysql_close($connection);
+  				}
+			} else {
+				echo '<h1>404 Not Found</h1><br/>
+				<b>Error message:</b> No validation code found!';
 			}
 			
-			if (isset($used) && $used == FALSE) {
-				if (isset($_SESSION['logged_in'])) {
-					// Update the code from unused to used
-					$query_update = "UPDATE tickets SET used = true WHERE code = '" . $code . "'";
-					mysql_query($query_update);
-				}
-				
-				// Code is correct
-				echo '
-		<script type="text/javascript">
-			// Create the QRCode
-			// new QRCode(document.getElementById("qrcode"), getParam("code"));
-			new QRCode(document.getElementById("qrcode"), {
-				text: getParam("code"),
-				width: 256,
-				height: 256,
-				colorDark : "#0000ff",
-				colorLight : "#ffffff",
-				correctLevel : QRCode.CorrectLevel.H
-			})
-		</script>
-				';
-			} elseif (isset($used) && $used == TRUE) {
-				// Code has been used before
-				echo '
-		<script type="text/javascript">
-			document.getElementById("qrcode").innerHTML = "Dieser Code wurde bereits verwendet!";
-		</script>
-				';
-			} else {
-				// Code is wrong	
-				echo '
-		<script type="text/javascript">
-			document.getElementById("qrcode").innerHTML = "Dein Code konnte nicht in der Datenbank gefunden werden. Bitte überprüfe deine Eingaben. Wenn du glaubst, dass hier ein Fehler vorliegt, melde dich bitte beim Veranstalter.";
-		</script>
-				';
-				
-			}
-	
-			// Close connection to MySQL server
-			mysql_close($connection);
 		?>
 
 		<footer>
