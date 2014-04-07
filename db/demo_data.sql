b4164012686c56f7186724d6da9b7ad5ebe6ec21
Marius Runde <marius.runde@uni-muenster.de>
2014-04-07 17:16:23 +0200

    Fix DB

--- db/demo_data.sql
+++ db/demo_data.sql
@@ -1,5 +1,5 @@
-INSERT INTO `ssbo-ticketing`.`tickets`
+INSERT INTO `ssbo_ticketing`.`tickets`
 (`code`, `firstname`, `lastname`, `street`, `housenumber`, `postalcode`, `town`, `date`)
 VALUES
 ('1911332118324935400', 'Max', 'Mustermann', 'Musterstraße', '1a', '12345', 'Musterdorf', '2014-02-07 16:02:41', '0'),
-('9751755374018103000', 'Erika', 'Mustermann', 'Musterweg', '9', '54321', 'Musterstadt', 2014-02-07 16:31:00', '1');
+('9751755374018103000', 'Erika', 'Mustermann', 'Musterweg', '9', '54321', 'Musterstadt', '2014-02-07 16:31:00', '1');
